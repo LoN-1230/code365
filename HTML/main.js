13f18bd9ac4d458943eb729faabe849ef5a15941
Lo'N <84562177+LoN-1230@users.noreply.github.com>
2022-08-08 11:31:25 +0800

    Add files via upload

---
+++ HTML/main.js
@@ -0,0 +1,8 @@
+console.log("Hello World!")
+let block_HTML = document.getElementById("block")
+const change_color = () => {
+    block_HTML.classList.remove("blue")
+    block_HTML.classList.add("red")
+    
+}
+
