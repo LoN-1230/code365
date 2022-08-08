fb69ab1f3f4f0f6b551852f8e35a3d8627e7edfd
Lo'N <84562177+LoN-1230@users.noreply.github.com>
2022-08-08 11:41:06 +0800

    Delete HTML directory

--- HTML/main.js
+++
@@ -1,8 +0,0 @@
-console.log("Hello World!")
-let block_HTML = document.getElementById("block")
-const change_color = () => {
-    block_HTML.classList.remove("blue")
-    block_HTML.classList.add("red")
-    
-}
-
